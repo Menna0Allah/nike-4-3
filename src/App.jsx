829a5e5e904eb4b56db7a45c8264cdc38ff17592
menna <menna.allah04@gmail.com>
2025-03-07 06:46:38 +0200

    make sections jsx, import them in index.js, put the logo in main index.js, make constants for constant data, finish Nav section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+import { 
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffers,
+  Subscribe,
+  SuperQuality 
+ } from './sections';
+ import Nav from './components/Nav';
+
+const App = () => (
+  <main className="relative">
+    <Nav/>
+    <section className="
+    xl:padding-1  wide:padding-r padding-b">
+      <Hero/>
+    </section>
+    <section className="padding">
+      <PopularProducts/>
+    </section>
+    <section className="padding">
+      <SuperQuality/>
+    </section>
+    <section className="Padding-x py-10">
+      <Services/>
+    </section>
+    <section className="Padding">
+      <SpecialOffers/>
+    </section>
+    <section className="padding bg-pale-blue">
+    <CustomerReviews/>
+    </section>
+    <section className="padding-x py-16 w-full">
+    <Subscribe/>
+    </section>
+    <section className="padding-x padding-t pb-8 bg-black">
+      <Footer/>
+    </section>
+  </main>
+);
+
+export default App;
