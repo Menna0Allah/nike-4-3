467908258ca6886e3dca51358129be3978673747
menna <menna.allah04@gmail.com>
2025-03-07 12:16:28 +0200

    finish the second part of hero and make it dynamic

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Button from '../components/Button'
 import { arrowRight } from '../assets/icons'
-import { statistics } from '../constants'
+import { shoes, statistics } from '../constants'
+import { bigShoe1 } from '../assets/images'
+import ShoeCard from '../components/ShoeCard';
+
+import { useState } from 'react'
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+
   return (
     <section 
       id="home"
@@ -12,7 +18,7 @@ const Hero = () => {
       <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28'>
         <p className='text-xl font-montserrat text-coral-red'>Our Winter Collection</p>
         <h1 className='mt-10 font-palanquin text-8xl max-sm:[72px] max-sm:leading-[82] font-bold'>
-          <span className='xl:bg-white xl:whitespace-nowrap relative z-110 pr-10'>The New Arrival</span>
+          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>The New Arrival</span>
           <br />
           <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
         </h1>
@@ -23,6 +29,27 @@ const Hero = () => {
             <div key={stat.label}>
               <p className='text-4xl font-palanquin font-bold'>{stat.value}</p>
               <p className='leading-7 font-montserrat text-slate-gray'>{stat.label}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
+        <img 
+          src={bigShoeImg} 
+          alt="show collection" 
+          width={610}
+          height={500}
+          className='object-contain relative z-10'
+        />
+        <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard
+                imgURL={shoe}
+                changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+                bigShoeImg={bigShoeImg}
+              />
             </div>
           ))}
         </div>
